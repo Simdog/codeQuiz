6e0d511925bfa684812dffab1baf62d50f30d8c6
Simeon Ogunyemi <oguny006@umn.edu>
2020-04-23 17:20:57 -0500

    started to add questions variable and working on having it run the question

--- newjs.js
+++ newjs.js
@@ -1,27 +1,58 @@
 //Global Scope
 var start = document.getElementById("startQuiz");
 var t = document.getElementById("timer");
-var timeLeft = 10;
-var socere = 0;
- var time = setInterval (startTimer, 1000);
+var timeLeft = 3;
+var score = 0;
+var hidden = document.querySelector(".hide");
+hidden.style.display = "none";
+var shuffledQuestions;
+var currentQuestions;
+var askQuestion = document.getElementById("questions");
 
-function startQ() {
+ 
+ 
+function setNextQuestion() {
+
+    askQuestion.innerHTML = questions[0].question;
+    console.log(questions[0]);
+}
+
+
+function startQ() { 
+    // hide the intorduction 
+    var hide = document.getElementById("hide");
+    if (hide.style.display === "none") {
+        hide.style.display = "block";
+        
+    } else {
+        hide.style.display = "none";
+        hidden.style.display = "block";
+        startTimer();
+        var time = setInterval (startTimer, 1000);
+    }
 //first redirect to questions page
- window.location.assign("questions.html");
+//  window.location.assign("questions.html");
 //then start the timer
+currentQuestions = 0;
+setNextQuestion();
 
 }
 
-function pageRedirect() {
+
+
+// function pageRedirect() {
     
-}
+// }
 
 function startTimer() {
     
-     
+    
 
-    if (timeLeft <= 0) {
-        clearInterval(time);
+    if (timeLeft < 0) {
+        clearInterval(startTimer);
+        timeLeft = ("0");
+
+
         
     } else {
         t.innerHTML = timeLeft;  
@@ -29,12 +60,15 @@ function startTimer() {
     }
 }
 
-const questions = [
+var questions = [
     {
         question: "what is 2 + 2", 
         answers: [
             {text: "4", correct: true},
-            {text: "69", correct: false}
+            {text: "69", correct: false},
+            {text: "22", correct: false},
+            {text : "2", correct: false}
+              
         ]
     }
 ]
